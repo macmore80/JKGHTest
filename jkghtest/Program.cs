e7151276df285e5b66c46f5cf48c768873702c70
Jongkwan Park <Jongkwan.Park@cognex.com>
2023-02-03 15:11:40 +0900

    modify code and rename text file

--- jkghtest/Program.cs
+++ jkghtest/Program.cs
@@ -6,11 +6,19 @@ namespace jkghtest
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Red;
+
             Console.WriteLine("Step 1. using github");
 
             Console.WriteLine("Step 2. commit, push, pull");
 
             Console.WriteLine("Step 2. checking a change after pullin from github to local pc");
+
+            //Console.ReadKey(); //developer-talk.tistory.com/320
+            Console.WriteLine("Push any key, please");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.WriteLine("What is Key?: " + keyInfo.Key);
 
 
         }
